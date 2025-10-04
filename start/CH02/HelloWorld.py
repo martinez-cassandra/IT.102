33af80c75c718b23f33ea19f45892d6db8418747
martinez-cassandra <martinez.cassandra@student.greenriver.edu>
2025-10-03 23:18:31 -0700

    added hello world to chapter 2

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by cassandra
+
+#Prompts the user for their name
+name = input ("Please enter your name: ")
+
+positive_affirmation = "This quarter is going to be the best!"
+#Print hello name?
+print(name)
+
+#Print a positive affirmation
+print(positive_affirmation)
+print("This is going to be amazing! ")
+
+# How do we prompt the user for there age?
+age = int(input("What is your age? "))
+
+# Print out in 2 years you will be xx years old?
+print(age + 2)
