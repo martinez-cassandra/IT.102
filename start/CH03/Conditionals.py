8de0e223f5328f6fb92b00ee743d482c7ef706a0
martinez-cassandra <martinez.cassandra@student.greenriver.edu>
2025-10-17 23:03:33 -0700

    created conditionals script

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
-# example workign with conditionals
-#By 
+# example working with conditionals
+#By Cassandra
+
+#First we have to gather if the day is good or bad, which requires user input
+question = input("Is today a good day? Please answer with y or n: ").lower()
+
+#Analyze the value and print of yes it is
+if question == 'y':
+    print('Yes it is')
+elif question == 'n':
+        print('Thats okay tomorrow will be ')
+elif question == 'idk' :
+    print('Thats okay')
+else:
+    print("Please enter a valid input of y or n for yes or no")
