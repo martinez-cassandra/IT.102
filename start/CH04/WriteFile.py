d7f4fc6c71d4a5e1c08cc4a94042637118009b38
martinez-cassandra <martinez.cassandra@student.greenriver.edu>
2025-11-04 00:00:33 -0800

    created write files script

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Cassandra
+
+'''
+Opens a file and write the content of response from certain questions
+'''
+
+#List of questions to ask the user
+questions = [
+    "What is your name? ",
+    "What is your favorite color? ",
+    "What is your first pets name? ",
+    "What is your mother's maiden name?",
+    "What elementary school did you attend? "
+
+]
+
+#Create a list to save responses
+answers = []
+
+#Loop through each question and save the outcome to answers
+for q in questions:
+     ans = input(q)
+     answers.append(ans)
+
+
+'''
+Write all answers to a file safely
+'''
+with open("hackme.txt", "w") as file:
+    for line in answers:
+        file.write(line + "\n")
