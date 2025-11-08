940c2bb9c28d74f336017342c95ee4057c730004
martinez-cassandra <martinez.cassandra@student.greenriver.edu>
2025-11-07 23:14:47 -0800

    Caesar Cipher

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By Cassandra
+
+# rot13 - caesar cipher with a shift of 13
+
+def rot13(text,shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            base - ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            result += char
+    return result
+
+message = input("input your text to rotate no spaces or numbers: ")
+shift = 13
+encrypted = rot13(message, shift)
+decrypted = rot13(encrypted, -shift)
+
+print(f"encrypted {encrypted}")
+print(f"decrypted {decrypted}")
