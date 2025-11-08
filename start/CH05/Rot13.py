4012957be16681c865ef4feabc7c83533fec8137
martinez-cassandra <martinez.cassandra@student.greenriver.edu>
2025-11-08 00:44:46 -0800

    Caesar Cipher

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -8,7 +8,7 @@ def rot13(text,shift):
     result = ""
     for char in text:
         if char.isalpha():
-            base - ord('A') if char.isupper() else ord('a')
+            base = ord('A') if char.isupper() else ord('a')
             result += chr((ord(char) - base + shift) % 26 + base)
         else:
             result += char
